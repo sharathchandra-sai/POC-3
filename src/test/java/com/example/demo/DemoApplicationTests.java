cae5260408ac13af2de8c72625d19a2cdcce8a4b
sharathchandra-sai <kodati.sharath2000@gmail.com>
2025-08-19 15:52:45 +0530

    Create DemoApplicationTests.java

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoApplicationTests {
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
