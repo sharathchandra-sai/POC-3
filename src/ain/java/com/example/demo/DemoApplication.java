f389cfe44113a8bf91c4c37681c7c03e86adc5c3
sharathchandra-sai <kodati.sharath2000@gmail.com>
2025-08-19 15:51:23 +0530

    Update DemoApplication.java

--- src/ain/java/com/example/demo/DemoApplication.java
+++ src/ain/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,11 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class DemoApplicationTests {
-    @Test
-    void contextLoads() {
-        assertTrue(true);
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
     }
 }
